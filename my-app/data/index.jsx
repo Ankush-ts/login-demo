151a1582329622435a0dcc42e146908e03884765
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2023-09-23 21:06:01 +0530

    first commit

---
+++ my-app/data/index.jsx
@@ -0,0 +1,18 @@
+
+export const data= [
+    {
+        username:"tony-stark",
+        password:"loveyou3000",
+        name: "Tony Howard Stark",
+        city:"New York",
+        age:35,
+    },
+    {
+        username:"bruce-banner",
+        password:"smash",
+        name: "Bruce Banner",
+        city:"California",
+        age:40,
+    },
+    
+];
