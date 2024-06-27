5b30d26239ecd1d59f0193da89b0a118a9fd8d8f
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2024-06-27 16:23:42 +0530

    Update Login.jsx

--- my-app/pages/Login.jsx
+++ my-app/pages/Login.jsx
@@ -3,57 +3,61 @@ import { useState } from "react";
 
 
 export default function Login() {
-    const [userName, setUserName]=useState("");
-    const [password, setPassword]=useState("");
-    const router=useRouter();
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
 
-    const formSubmit= async  (event) => { 
+    const formSubmit = async (event) => {
         event.preventDefault();
-        try{
+        try {
+
+            // const res=await fetch('/api/Login')
+            // console.log('api called from the client');
+
             const res = await fetch("/api/Login", {
-                body: JSON.stringify({userName, password}),
-                method:"POST",
+                body: JSON.stringify({ username, password }),
+                method: "POST",
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
                 },
             });
-            const jsonResponse= res.json();
+            const jsonResponse = res.json();
             // console.log(jsonResponse);
             router.push("/");
         }
-        
-        catch(e){
+
+        catch (e) {
             console.error(e)
         }
-        
+
     };
 
     return (
-      <div className="container">
-        <form onSubmit={formSubmit}>
-            <div className="form-group">
-                <label htmlFor="username">UserName</label>
-                <input type="text"
-                className="form-control"
-                id="username"
-                placeholder="Enter UserName"
-                onChange={(e)=>setUserName(e.target.value)}
-                />
-                
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password"
-                className="form-control"
-                id="password"
-                placeholder="Enter Password"
-                onChange={(e)=>setPassword(e.target.value)}
-                />
-                
-            </div>
-            <button className="btn btn-primary mt-3">Login</button>
-        </form>
-      </div>
+        <div className="container">
+            <form onSubmit={formSubmit}>
+                <div className="form-group mt-3">
+                    <label htmlFor="username">Username</label>
+                    <input type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Enter Username"
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+
+                </div>
+                <div className="form-group mt-3">
+                    <label htmlFor="password">Password</label>
+                    <input type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Enter Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                </div>
+                <button className="btn btn-primary mt-3">Login</button>
+            </form>
+        </div>
     );
-  }
+}
